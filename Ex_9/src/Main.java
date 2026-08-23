78e3c12f384eda725956e5b9a02dc2ee46467add
João Pedro Fonseca Baião <joaojpfbaiao@gmail.com>
2026-08-23 18:18:20 -0300

    resolução do exercício 9

---
+++ Ex_9/src/Main.java
@@ -0,0 +1,27 @@
+void main() {
+    Scanner input = new Scanner(System.in);
+    int[] numeros = new int[6];
+    int somaPares = 0;
+    int quantidadeImpares = 0;
+    for (int i = 0; i < numeros.length; i++) {
+        System.out.println("Digite o " + (i + 1) + "º número:");
+        numeros[i] = input.nextInt();
+    }
+    System.out.println("RELATÓRIO");
+    System.out.println("Os números pares são:");
+    for (int i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 == 0) {
+            System.out.println("Número " + numeros[i] + " na posição " + (i + 1));
+            somaPares += numeros[i];
+        }
+    }
+    System.out.println("Soma dos pares = " + somaPares);
+    System.out.println("Os números ímpares são:");
+    for (int i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 != 0) {
+            System.out.println("Número " + numeros[i] + " na posição " + (i + 1));
+            quantidadeImpares++;
+        }
+    }
+    System.out.println("Quantidade de ímpares = " + quantidadeImpares);
+}
